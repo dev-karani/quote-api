8b6079d531c89c540f15c733e38237318e048079
dev-karani <edwin.k.kithinji@gmail.com>
2025-11-14 17:58:47 +0300

    git init

---
+++ main.js
@@ -0,0 +1,10 @@
+const {
+  app
+} = require("./app.js");
+
+// designate which PORT the server will listen on
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, ()=>{
+  console.log(`server is running on port ${Port}`)
+})
